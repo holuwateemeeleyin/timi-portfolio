f74db871c3ab11b058a61b7c2d4f447d026e9d7b
Oluwatimileyin Samuel <abegundeosamuel@gmail.com>
2022-09-24 14:31:47 +0100

    Sidear

---
+++ src/Header/HeaderMobile/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SideNav } from 'react-simple-sidenav'
+import { IoMdClose } from 'react-icons/io'
+import SidebarItems from './SidebarItems'
+
+export default function SideBar({ showNav, onHideNav }) {
+  return (
+    <SideNav
+        showNav={showNav}
+        onHideNav={onHideNav}
+        navStyle={{
+            background:'#242',
+            maxWidth:'250px'
+        }}
+    >
+        <div className='close-nav'>
+            <IoMdClose 
+                color='#fff' 
+                size={22}
+                onClick={onHideNav}
+                />
+        </div>
+      <SidebarItems/>
+    </SideNav>
+  )
+}
