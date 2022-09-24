6a1fad3494df0069b8d74a0323e0845280e3ab44
Oluwatimileyin Samuel <abegundeosamuel@gmail.com>
2022-09-24 14:28:10 +0100

    Header for mobile screen

---
+++ src/Header/HeaderMobile/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './Header.css'
+import { HiOutlineMenuAlt2 } from 'react-icons/hi'
+import SideBar from './SideBar'
+
+export default function Header() {
+  const [ showNav, setShowNav ] = useState(false)
+  
+  const onHideNav = ()=> {
+    setShowNav(false)
+  }
+  return (
+    <div className='header-mobile'>
+        <div className='sidebar'>
+          <HiOutlineMenuAlt2 size={22}
+            onClick={()=> setShowNav(true)}
+          />
+        </div>
+        <SideBar
+          showNav={showNav}
+          onHideNav={()=> onHideNav()}
+        />
+        <div className='header-title'>
+          DG
+        </div>
+    </div>
+  )
+}
